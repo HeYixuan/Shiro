575e0e3df31eaa9d85c9168ab1a51844f5a539bb
HeYixuan <15517551511@126.com>
2016-07-15 18:11:40 +0800

    提交导入导出Excel类

---
+++ Maven-MVC/src/main/java/org/springframe/utils/ExportExcel.java
@@ -0,0 +1,10 @@
+package org.springframe.utils;
+
+/**
+ * 导出Excel文件（导出“XLSX”格式，支持大数据量导出   @see org.apache.poi.ss.SpreadsheetVersion）
+ * @author HeYixuan
+ * @version 2016-07-15
+ */
+public class ExportExcel {
+
+}
---
+++ Maven-MVC/src/main/java/org/springframe/utils/ImportExcel.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package org.springframe.utils;
+
+/**
+ * 导入Excel文件（支持“XLS”和“XLSX”格式）
+ * @author HeYixuan
+ * @version 2016-07-15
+ */
+public class ImportExcel {
+
+}
